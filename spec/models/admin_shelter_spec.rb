2a11ba9493d1fb8d39bbdbe3a9569da43754f8b2
Alex M <alexm0717@hotmail.com>
2022-10-24 19:23:25 -0700

    story ten complete

---
+++ spec/models/admin_shelter_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+
+RSpec.describe(AdminShelter) do
+  before(:each) do
+    @shelter1 = Shelter.create(    name: "Als Adoption",     city: "Apple valley CA",     foster_program: false,     rank: 9)
+    @shelter3 = Shelter.create(    name: "Calcity Center",     city: "Catalina CA",     foster_program: false,     rank: 9)
+    @shelter2 = Shelter.create(    name: "Barstow Building",     city: "Barstow CA",     foster_program: false,     rank: 9)
+  end
+
+  describe("class methods") do
+    describe("#alpha_organize") do
+      it("returns alphabetical order") do
+        expect(Shelter.alpha_organize).to eq([@shelter3, @shelter2, @shelter1])
+      end
+    end
+  end
+end
